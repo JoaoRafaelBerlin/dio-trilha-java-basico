95c4ba8e97361b1cb15bced13e4a7fa60a669116
RafaelBerlin <jrafonseca21@hotmail.com>
2023-03-17 21:47:38 +0100

    dio-trilha-java-basico

---
+++ sistema-smar-tv/src/Calculadora.java
@@ -0,0 +1,10 @@
+//*A calculadora realiza operacoes matematicas entre numeros inteiros*/
+
+public class Calculadora {
+
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+        
+    }
+    
+}
---
+++ sistema-smar-tv/src/Comentarios.java
@@ -0,0 +1,30 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Ola, eu sou um comentario em uma unica linha
+        /*Ola, eu sou um 
+        comentario que posso 
+        ser mais detralhado */
+
+        /**Estas duas estrelinhas acima
+        * sao para identificar que voce 
+        * pretende elaborar um comentario
+        * comparado a documentacao
+        * que incrivel!!!
+        */
+    }
+
+    public int someMultiplica (int n, int x, String m){
+        int r = 0; //r é igual ao resultado
+        if (m == "M"){ // M é igual multiplicacao
+            r = n * x;
+        }else{
+        // se nao soma mesmo
+        return r;
+        }
+        return r;
+
+    }
+
+    }
+    
+    
